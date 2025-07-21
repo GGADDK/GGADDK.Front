25bdfa190f80b738d779abca3fb704d22b60e5e7
bitbyte08 <me@bitworkspace.kr>
2025-07-22 07:51:47 +0900

    feat :: RightSideBar 프레임 작성

---
+++ src/shared/components/RightSidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RightSidebar: React.FC = () => {
+  return (
+    <aside className="w-85 h-full border-l-1">
+      RightSidebar
+    </aside>
+  )
+}
+
+export default RightSidebar;
